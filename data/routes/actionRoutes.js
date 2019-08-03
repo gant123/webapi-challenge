3aeebf443e97be20e03f9cd6507965de5f7cf9a7
Robert Gant <demontegant@gmail.com>
2019-08-03 11:47:51 -0500

    added all routes, working on stretch chal now

---
+++ data/routes/actionRoutes.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const db = require('../helpers/actionModel');
+const router = express.Router();
+
+/******************************************************************************
+ *                            Custom middleware                               *
+ ******************************************************************************/
+
+function projectIdCheck(req, res, next) {
+  console.log(req.body);
+  if (!req.body.project_id) {
+    res.status(400).json({
+      message: `There is no project ID for the action`
+    });
+  } else {
+    next();
+  }
+}
+
+/******************************************************************************
+ *                            End Points                                      *
+ ******************************************************************************/
+
+// get all actions
+router.get('/', (req, res) => {
+  db.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ err: 'Could not get actions' });
+    });
+});
+
+// get action by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const action = await db.get(req.params.id);
+
+    if (action) {
+      res.status(200).json(action);
+    } else {
+      res.status(404).json({ message: 'Action not found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the action'
+    });
+  }
+});
+
+// add new action
+router.post('/', projectIdCheck, async (req, res) => {
+  try {
+    const action = await db.insert(req.body);
+    res.status(201).json(action);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the action'
+    });
+  }
+});
+
+// update action
+router.put('/:id', async (req, res) => {
+  try {
+    const action = await db.update(req.params.id, req.body);
+    if (action) {
+      res.status(200).json(action);
+    } else {
+      res.status(404).json({ message: 'The action could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the action'
+    });
+  }
+});
+
+// delete an action
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'The action has been nuked' });
+    } else {
+      res.status(404).json({ message: 'The action could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the action'
+    });
+  }
+});
+
+module.exports = router;
---
+++ data/routes/projectRoutes.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const db = require('../helpers/projectModel');
+const router = express.Router();
+
+/******************************************************************************
+ *                            End Points                                      *
+ ******************************************************************************/
+
+// get all projects
+router.get('/', (req, res) => {
+  db.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({ err: 'Could not get projects' });
+    });
+});
+
+/******************************************************************************
+ *                            Get Project by Id                               *
+ ******************************************************************************/
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await db.get(req.params.id);
+
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: 'Project not found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the project'
+    });
+  }
+});
+
+/******************************************************************************
+ *                            Add new project                                 *
+ ******************************************************************************/
+router.post('/', async (req, res) => {
+  try {
+    const project = await db.insert(req.body);
+    res.status(201).json(project);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the project'
+    });
+  }
+});
+
+/******************************************************************************
+ *                            Update project                                  *
+ ******************************************************************************/
+router.put('/:id', async (req, res) => {
+  try {
+    const project = await db.update(req.params.id, req.body);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: 'The project could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the project'
+    });
+  }
+});
+
+/******************************************************************************
+ *                            Delete project                                  *
+ ******************************************************************************/
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'The project has been nuked' });
+    } else {
+      res.status(404).json({ message: 'The project could not be found' });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the project'
+    });
+  }
+});
+/******************************************************************************
+ *         add an endpoint that returns all the actions for a project         *
+ ******************************************************************************/
+//only id 1 has actions at the moment
+router.get('/:id/actions', async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const actions = await db.getProjectActions(req.params.id);
+    res.status(200).json(actions);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error getting the actions for the project'
+    });
+  }
+});
+
+module.exports = router;
